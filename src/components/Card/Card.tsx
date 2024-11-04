d9f9e46328d0d5ce4812d95561252ce7cc18dd97
Andrey Pravosudov <studiodec8@gmail.com>
2024-11-04 13:43:36 +0300

    fix Card and MultiDropdown components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,66 +1,67 @@
 import React from 'react';
-import styles from './Card.module.css';
+import s from './Card.module.scss';
+import cn from 'classnames';
 
-import Text from '../Text';
+import Text from 'components/Text';
 
 export type CardProps = {
-    /** Дополнительный classname */
-    className?: string,
-    /** URL изображения */
-    image: string;
-    /** Слот над заголовком */
-    captionSlot?: React.ReactNode;
-    /** Заголовок карточки */
-    title: React.ReactNode;
-    /** Описание карточки */
-    subtitle: React.ReactNode;
-    /** Содержимое карточки (футер/боковая часть), может быть пустым */
-    contentSlot?: React.ReactNode;
-    /** Клик на карточку */
-    onClick?: React.MouseEventHandler;
-    /** Слот для действия */
-    actionSlot?: React.ReactNode;
+  /** Дополнительный classname */
+  className?: string;
+  /** URL изображения */
+  image: string;
+  /** Слот над заголовком */
+  captionSlot?: React.ReactNode;
+  /** Заголовок карточки */
+  title: React.ReactNode;
+  /** Описание карточки */
+  subtitle: React.ReactNode;
+  /** Содержимое карточки (футер/боковая часть), может быть пустым */
+  contentSlot?: React.ReactNode;
+  /** Клик на карточку */
+  onClick?: React.MouseEventHandler;
+  /** Слот для действия */
+  actionSlot?: React.ReactNode;
 };
 
-const Card: React.FC<CardProps> = (
-    {
-        className,
-        image,
-        captionSlot,
-        title,
-        subtitle,
-        contentSlot,
-        onClick,
-        actionSlot,
-        ...otherProps }
-) => {
-
-    return (
-        <div
-            className={`${styles.card} ${className}`}
-            onClick={onClick}
-            {...otherProps}
-        >
-            <div className={styles.card__header}>
-                <img className={styles.card__img} src={image} alt={'->' + image} />
-            </div>
-            <div className={styles.card__body}>
-                {captionSlot && <div className={styles.card__captionSlot}>
-                    <Text view={'p-14'} color={'secondary'} weight={'medium'}>{captionSlot}</Text>
-                </div>}
-                <div className={styles.card__titleSlot}>
-                    <Text tag={'h4'} view={'p-20'} weight={'medium'} maxLines={2}>{title}</Text>
-                </div>
-                <div className={styles.card__subtitleSlot}>
-                    <Text view={'p-16'} color={'secondary'} maxLines={3}>{subtitle}</Text>
-                </div>
-                <div className={styles.card__footer}>
-                    {contentSlot && <Text tag={'div'} view={'p-18'} weight={'bold'}>{contentSlot}</Text>}
-                    {actionSlot}
-                </div>
-            </div>
+const Card: React.FC<CardProps> = ({
+  className,
+  image,
+  captionSlot,
+  title,
+  subtitle,
+  contentSlot,
+  onClick,
+  actionSlot,
+  ...otherProps
+}) => {
+  return (
+    <div className={cn(s.card, className)} onClick={onClick} {...otherProps}>
+      <div className={s.card__header}>
+        <img className={s['card__header-src']} src={image} alt={'card'} />
+      </div>
+      <div className={s.card__body}>
+        {captionSlot && (
+          <Text className={s.card__caption} view="p-14" weight="medium" color="secondary">
+            {captionSlot}
+          </Text>
+        )}
+        <Text maxLines={2} tag="h4" view="p-20" weight="medium" color="primary">
+          {title}
+        </Text>
+        <Text maxLines={3} className={s.card__subtitle} view="p-16" color="secondary">
+          {subtitle}
+        </Text>
+        <div className={s.card__footer}>
+          {contentSlot && (
+            <Text view="p-18" weight="bold" className={s.card__content}>
+              {contentSlot}
+            </Text>
+          )}
+          <div className={s.card__action}>{actionSlot}</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/MultiDropdown/MultiDropdown.tsx
+++ src/components/MultiDropdown/MultiDropdown.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
-import s from './MultyDropdown.module.scss';
+import s from './MultiDropdown.module.scss';
 import cn from 'classnames';
 import Input from '../Input';
-import Text from 'components/Text'
+import Text from 'components/Text';
 import ArrowDownIcon from 'components/icons/ArrowDownIcon';
 
 export type Option = {
@@ -23,7 +23,7 @@ export type MultiDropdownProps = {
   onChange: (value: Option[]) => void;
   /** Заблокирован ли дропдаун */
   disabled?: boolean;
-  /** Возвращает строку которая будет выводится в инпуте. 
+  /** Возвращает строку которая будет выводится в инпуте.
    * В случае если опции не выбраны, строка должна отображаться как placeholder. */
   getTitle: (value: Option[]) => string;
 };
@@ -36,57 +36,52 @@ const MultiDropdown: React.FC<MultiDropdownProps> = ({
   disabled = false,
   getTitle,
 }) => {
-
   const wrapperRef = React.useRef<HTMLDivElement>(null);
   const ref = React.useRef<HTMLInputElement>(null);
   const [filter, setFilter] = React.useState('');
   const [isOpened, setIsOpened] = React.useState(false);
-  
+
   const open = () => {
     setIsOpened(true);
   };
-  
+
   React.useEffect(() => {
     const handlerClick = (e: MouseEvent) => {
       if (!wrapperRef.current?.contains(e.target as HTMLDivElement)) {
         setIsOpened(false);
       }
     };
-  
-    window.addEventListener("click", handlerClick);
-  
+
+    window.addEventListener('click', handlerClick);
+
     return () => {
-      window.removeEventListener("click", handlerClick);
-    }
+      window.removeEventListener('click', handlerClick);
+    };
   }, []);
-  
+
   React.useEffect(() => {
     if (isOpened) {
       setFilter('');
     }
   }, [isOpened]);
-  
+
   const title = React.useMemo(() => getTitle(value), [getTitle, value]);
-  
+
   const isEmpty = value.length === 0;
 
   const filteredOptions = React.useMemo(() => {
     const str = filter.toLocaleLowerCase();
     return options.filter((o) => o.value.toLocaleLowerCase().indexOf(str) === 0);
   }, [filter, options]);
-  
-  const selectedKeysSet = React.useMemo<Set<Option['key']>>(
-    () => 
-      new Set(value.map(({ key }) => key)),
-    [value]
-  );
-  
+
+  const selectedKeysSet = React.useMemo<Set<Option['key']>>(() => new Set(value.map(({ key }) => key)), [value]);
+
   const onSelect = React.useCallback(
     (option: Option) => {
       if (disabled) {
         return;
       }
-  
+
       if (selectedKeysSet.has(option.key)) {
         onChange([...value].filter(({ key }) => key !== option.key));
       } else {
@@ -95,134 +90,43 @@ const MultiDropdown: React.FC<MultiDropdownProps> = ({
 
       ref.current?.focus();
     },
-    [disabled, onChange, value, selectedKeysSet]
+    [disabled, onChange, value, selectedKeysSet],
   );
-    
+
   const opened = isOpened && !disabled;
 
-return (
-<div
-  ref={wrapperRef}
-  className={cn(
-    s['multi-dropdown'],
-    className,
-  )}
->
-  <Input
-    onClick={open}
-    ref={ref}
-    disabled={disabled}
-    placeholder={title}
-    className={s['multi-dropdown__field']}
-    value={opened ? filter : isEmpty ? '' : title}
-    onChange={setFilter}
-    afterSlot={<ArrowDownIcon color="secondary" />}
-  />
-  {opened && (
-    <div className={s['multi-dropdown__options']}>
-      {filteredOptions.map((option) => (
-        <button
-          className={cn(
-            s['multi-dropdown__option'],
-            selectedKeysSet.has(option.key) && s['multi-dropdown__option_selected'],
-          )}
-          key={option.key}
-          onClick={() => {onSelect(option)}}
-        >
-          <Text view="p-16">
-            {option.value}
-          </Text>
-        </button>
-      ))}
+  return (
+    <div ref={wrapperRef} className={cn(s['multi-dropdown'], className)}>
+      <Input
+        onClick={open}
+        ref={ref}
+        disabled={disabled}
+        placeholder={title}
+        className={s['multi-dropdown__field']}
+        value={opened ? filter : isEmpty ? '' : title}
+        onChange={setFilter}
+        afterSlot={<ArrowDownIcon color="secondary" />}
+      />
+      {opened && (
+        <div className={s['multi-dropdown__options']}>
+          {filteredOptions.map((option) => (
+            <button
+              className={cn(
+                s['multi-dropdown__option'],
+                selectedKeysSet.has(option.key) && s['multi-dropdown__option_selected'],
+              )}
+              key={option.key}
+              onClick={() => {
+                onSelect(option);
+              }}
+            >
+              <Text view="p-16">{option.value}</Text>
+            </button>
+          ))}
+        </div>
+      )}
     </div>
-  )}
-</div>
-
-);
-
-  // const [isOpen, setIsOpen] = useState(false);
-  // const [filter, setFilter] = useState<Option[]>(options)
-  // const [inputValue, setInputValue] = useState('');
-  // const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-  // const dropdownRef = useRef<HTMLDivElement>(null);
-
-
-  // useEffect(() => {
-  //   setFilter(options);
-  // }, [options]);
-
-
-  // // Обработка клика вне компонента для закрытия списка
-  // useEffect(() => {
-  //   const handleClickOutside = (event: MouseEvent) => {
-  //     if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-  //       setIsOpen(false);
-  //     }
-  //   };
-  //   document.addEventListener('mousedown', handleClickOutside);
-  //   return () => {
-  //     document.removeEventListener('mousedown', handleClickOutside);
-  //   };
-  // }, []);
-
-  // const handleInputChange = (currentValueString: string) => {
-  //   setInputValue(currentValueString);
-
-  //   const split: string[] = currentValueString.split(',');
-  //   const possibleOptions: Set<Option> = new Set();
-
-  //   for (const elem of split) {
-  //     const possibleOptionFound = options.filter(option => option.value.includes(elem))
-  //     possibleOptionFound.forEach(option => possibleOptions.add(option));
-  //   }
-  //   setFilter(Array.from(possibleOptions))
-  // };
-
-  // const handleOptionClick = (option: Option) => {
-  //   if (value.some(selected => selected.key === option.key)) {
-  //     // Убираем опцию из выбранных, если она уже выбрана
-  //     onChange(value.filter(selected => selected.key !== option.key));
-  //   } else {
-  //     // Добавляем опцию к выбранным
-  //     onChange([...value, option]);
-  //   }
-  // };
-
-  // const renderOption = (option: Option, index: number) => (
-  //   <div
-  //     key={option.key}
-  //     onClick={() => handleOptionClick(option)}
-  //     onMouseEnter={() => setHoveredIndex(index)}
-  //     className={`${styles.option} ${value.some(selected => selected.key === option.key) ? styles.selectedOption : ''} ${hoveredIndex === index ? 'hovered' : ''}`}
-  //   >
-  //     {option.value}
-  //   </div>
-  // );
-
-  // return (
-  //   <div className={`${styles.multiDropdownContainer} ${className}`} ref={dropdownRef}>
-  //     <Input
-  //       placeholder={getTitle([])}
-  //       value={value.length > 0 ? getTitle(value) : inputValue}
-  //       onClick={() => !disabled && value.length > 0 ? setIsOpen(!isOpen) : !isOpen && setIsOpen(true)}
-  //       onChange={(v) => handleInputChange(v)}
-  //       disabled={disabled}
-  //       afterSlot={<ArrowDownIcon color="secondary" />}
-  //       // className={styles.input}
-
-  //     />
-  //     {isOpen && !disabled && (
-  //       <div className={styles.dropdownList}>
-  //         {options.length > 0 ? (
-  //           filter.map(renderOption)
-  //         ) : (
-  //           <div className={styles.noOptions} >Нет доступных опций</div>
-  //         )}
-  //       </div>
-  //     )}
-  //   </div>
-  // );
+  );
 };
 
 export default MultiDropdown;
