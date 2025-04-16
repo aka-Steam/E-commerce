f136814914977d7d4987846b5ad8cd92df334b46
Andrey Pravosudov <studiodec8@gmail.com>
2025-04-16 13:10:16 +0300

    fix: BrowserRouter has been changed to HashRouter to work correctly with github-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'regenerator-runtime'
+import 'regenerator-runtime';
 
-import { initTheme } from 'utils/theme'
+import { initTheme } from 'utils/theme';
 import 'styles/index.scss';
 import 'configs/configureMobX.ts';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 initTheme(); // Инициализация темы при загрузке приложения
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
-)
+);
 
-if(import.meta.webpackHot){
+if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
 }
